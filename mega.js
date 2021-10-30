7214858be9d741c1559c6b4f6daeed08b35be3c4
Willy Biti <61201244+Willy-biti@users.noreply.github.com>
2021-10-29 21:52:23 -0300

    Testando JS

--- mega.js
+++ mega.js
@@ -1,3 +1,9 @@
-function mega(){
-    let testando = "Willy"
+function digitar(){
+    alert("Willy o cara mais foda");
 }
+
+function testar(valor1, valor2){
+    return valor1, valor2
+}
+
+console.log(testar(1, 4))
