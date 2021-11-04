eed5ca28260212a43e756a9d973578743aec9619
Willy Biti <61201244+Willy-biti@users.noreply.github.com>
2021-11-03 21:32:40 -0300

    Mexendo no script

--- mega.js
+++ mega.js
@@ -1,9 +1,33 @@
-function digitar(){
-    alert("Willy o cara mais foda");
+// function digitar(){
+//     alert("Willy o cara mais foda");
+// }
+
+// function testar(valor1, valor2){
+//     return valor1, valor2
+// }
+
+// console.log(testar(1, 4))
+// window.alert("nada")
+// window.prompt("digite seu nome")
+
+
+// alert("sim")
+
+/*let banner = document.querySelector('.foto-banners-second-pub-img-1')
+
+function testar(){
+    banner.style.display = 'none'
 }
 
-function testar(valor1, valor2){
-    return valor1, valor2
+testar();*/
+
+
+let noticias = document.querySelector('.titulo-pub-conteudo-h1 h1');
+let imgBanner = document.querySelector('.foto-banners-principal img')
+
+function ultima(){
+    noticias.style.display = 'none';
+    imgBanner.style.display = 'none';
 }
 
-console.log(testar(1, 4))
+ultima();
