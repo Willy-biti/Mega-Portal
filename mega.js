23e12cf4ebb9f6a24d7039a2675882af42e53955
Willy Biti <61201244+Willy-biti@users.noreply.github.com>
2021-11-12 19:29:54 -0300

    Scroll
    Foi adicionado botão scroll

--- mega.js
+++ mega.js
@@ -1,33 +1,30 @@
-// function digitar(){
-//     alert("Willy o cara mais foda");
-// }
-
-// function testar(valor1, valor2){
-//     return valor1, valor2
-// }
-
-// console.log(testar(1, 4))
-// window.alert("nada")
-// window.prompt("digite seu nome")
-
-
-// alert("sim")
-
-/*let banner = document.querySelector('.foto-banners-second-pub-img-1')
-
-function testar(){
-    banner.style.display = 'none'
-}
-
-testar();*/
-
-
 let noticias = document.querySelector('.titulo-pub-conteudo-h1 h1');
-let imgBanner = document.querySelector('.foto-banners-principal img')
+let imgBanner = document.querySelector('.foto-banners-second-pub-img-1 img')
 
 function ultima(){
-    noticias.style.display = 'none';
-    imgBanner.style.display = 'none';
+    // noticias.innerHTML = 'Willy Biti';
+    // imgBanner.style.display = 'none';
 }
 
+
 ultima();
+
+
+
+function subirTela(){
+    //SCROLL = ROLAGEM
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' //BEHAVIOR= COMPORTEMENTO
+    })
+}
+
+function botaoSumir(){
+    if(window.scrollY === 0){
+        document.querySelector('.botao-subir').style.display = 'none';
+    } else{
+        document.querySelector('.botao-subir').style.display = 'block';
+    }
+}
+
+window.addEventListener('scroll', botaoSumir)
